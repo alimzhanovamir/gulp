ded5a8618f747d2a8b7863abefceda996e031053
Alimzhanov Amir <alimzhanovamir@users.noreply.github.com>
2018-09-10 13:09:16 +0300

    Update, add gulp-nunjuks-render

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,9 @@
-var gulp				= require('gulp'),
-		sass				= require('gulp-sass'),
-		combinemq		= require('gulp-combine-mq'),
-		browserSync	= require('browser-sync'),
-		autopref		= require('gulp-autoprefixer');
+var gulp						= require('gulp'),
+		sass						= require('gulp-sass'),
+		combinemq				= require('gulp-combine-mq'),
+		browserSync			= require('browser-sync'),
+		autopref				= require('gulp-autoprefixer'),
+		nunjuksRender   = require('gulp-nunjucks-render');
 
 // Task SASS
 gulp.task('sass', function(){
@@ -18,15 +19,25 @@ gulp.task('sass', function(){
 gulp.task('browser-sync', function(){
 	browserSync({
 		server: {
-			baseDir:'src'
+			baseDir: 'src'
 		},
-		notify:false
+		notify: false
 	})
 });
 
+// Task Nunjuks
+gulp.task('nunjucks', function() {
+	return gulp.src('src/njk/*.+(html|njk)')
+		.pipe(nunjuksRender({
+			path: 'src/njk/'
+		}))
+		.pipe(gulp.dest('src'));
+});
+
 // Task WATCH
-gulp.task('watch', ['browser-sync', 'sass'], function(){
+gulp.task('watch', ['nunjucks', 'browser-sync', 'sass'], function(){
+	gulp.watch('src/**/*.+(html|njk)', ['nunjucks'])
 	gulp.watch('src/sass/*.sass', ['sass']);
-	gulp.watch('src/*.html', browserSync.reload);
+	gulp.watch('src/**/*.+(html|njk)', browserSync.reload);
 	gulp.watch('src/**/*.js', browserSync.reload);
 });
